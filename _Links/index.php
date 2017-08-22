c6b577ae7f2f53ace752095f9fb6811004a25cf9
yarg666 <julienmargelin@gmail.com>
2017-08-22 12:17:51 +0200

    init

---
+++ _Links/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd"> 
+
+
+
+<html>
+<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+		<link rel="shortcut icon" href="favicon.ico">
+		<meta http-equiv="Content-Language" content="fr"/>
+		<title>bonjour</title>
+		
+		
+<!-- CSSSSSSSSSSSSSSSSSSSSSSSSSS -->
+
+<style type="text/css">
+		
+html {height: 100%;width: 100%;overflow: auto;overflow-y: scroll;}
+
+a {border:0;margin:0;padding:0;outline:none;}
+
+img {margin:0;padding:0;border:0;height:400px;}
+		
+body {position:absolute;width:900px;height:400px;left:50%;top:50%;margin-left:-450px;
+margin-top:-200px;}
+
+#bonjour {position:absolute;left:0px;top:0px;cursor: pointer;}
+
+object {cursor: pointer;}
+
+
+</style>
+
+
+</head>
+
+<body>
+
+<!-- titre -->
+
+<a href="http://www.3d-meier.de/ ">http://www.3d-meier.de/  Database de formule mathématique pour créer des vectoriels field cf entagma</a> 
+<br>
+<a href="https://berniebernie.fr/wiki/Main_Page"> https://berniebernie.fr/wiki/Main_Page Wiki sur houdini, python, vex</a> 
+<br>
+<a href="http://www.deborahrfowler.com/index.html  "> http://www.deborahrfowler.com/index.html Cour sur houdini, avec des thèses d’etudiants </a> 
+<br>
+<a href="http://www.tokeru.com/cgwiki/index.php?title=Main_Page"> Le fameux cg Wiki de Tokeru </a>
+<br>
+<a href="https://houdinitricks.com/"> https://houdinitricks.com/ </a>
+	
+	
+	
+</div>
+</body>
+</html>
